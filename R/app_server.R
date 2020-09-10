a30b3cfd84af883be0d90dd7f2a21db2c27aca10
Z3tt <cedricphilippscherer@gmail.com>
2020-09-10 10:56:56 +0200

    :lipstick: clean-up

--- R/app_server.R
+++ R/app_server.R
@@ -1,6 +1,6 @@
 #' The application server-side
-#' 
-#' @param input,output,session Internal parameters for {shiny}. 
+#'
+#' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @import fullPage
@@ -8,13 +8,12 @@
 app_server <- function( input, output, session ) {
   # List the first level callModules here
   output$caption <- typed::renderTyped({
-    #typed::typed(c("Freedom of Fake news^1000", "Freedom of Press Index^500<br>A Visualisation"), typeSpeed = 25, smartBackspace = TRUE)
     typed::typed(c("A contribution to the CorrelAid X Challenge 2020"), typeSpeed = 15)
   })
-  
-  tmap::tmap_options(basemaps = c(`Light Basemap` = "CartoDB.PositronNoLabels", `Dark Basemap` = "CartoDB.DarkMatterNoLabels", `Colored Basemap` = "CartoDB.VoyagerNoLabels"), 
+
+  tmap::tmap_options(basemaps = c(`Light Basemap` = "CartoDB.PositronNoLabels", `Dark Basemap` = "CartoDB.DarkMatterNoLabels", `Colored Basemap` = "CartoDB.VoyagerNoLabels"),
                      overlays = c(Labels = "CartoDB.PositronOnlyLabels"))
-  
+
   callModule(mod_intro_server, "intro")
   callModule(mod_cities_server, "cities")
   callModule(mod_districts_server, "districts")
